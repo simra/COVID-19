027886bb6a131f85af1cf6b47a76a123b390bd60
Robert Sim <robert.sim@gmail.com>
2020-11-28 23:57:21 -0800

    11-28 checkin

---
+++ update.sh
@@ -0,0 +1,7 @@
+git pull jhu master
+cd other3p
+`head -1 README.md`
+#cd ../notebooks
+#/c/Users/rsim/Anaconda3/Scripts/activate
+#export PYTHONPATH=/c/Users/rsim/Anaconda3/Library:/c/Users/rsim/Anaconda3/lib:/c/Users/rsim/Anaconda3/bin:/c/Users/rsim/Anaconda3/Scripts:/c/Users/rsim/Anaconda3/Library/bin
+#/c/Users/rsim/Anaconda3/python runNotebook.py "COVID-19 Plots -Facebook.ipynb"
